4a9054fd2459bedbff6542df1362e15ef6d1ae8d
Morteza Rezanejad <morteza.rezanejad@gmail.com>
2022-03-04 16:38:12 -0500

    t

--- test_scripts/test_junctions.m
+++ test_scripts/test_junctions.m
@@ -4,4 +4,6 @@ LD1 = LD(1);
 vecLD.numContours = LD1.numLines;
 vecLD.contours = LD1.lines;
 
-vecLD = computeJunctions(vecLD)
+% vecLD = computeJunctions(vecLD);
+vecLD = computeContourProperties(vecLD);
+vecLD = computeCurvature(vecLD);
