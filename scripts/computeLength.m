3c43f6518c71bd44429b2ca8c9fb39f9298b1fac
DirkBWalther <dirk.walther@gmail.com>
2022-03-04 14:02:29 -0500

    compute length

--- scripts/computeLength.m
+++ scripts/computeLength.m
@@ -1,6 +1,6 @@
 function vecLD = computeLength(vecLD)
 % vecLD = computeLength(vecLD,whichProps)
-%         computes orientations for the contours in the vectorized line
+%         computes lentgh for the contours in the vectorized line
 %         drawing vecLD
 %         Note that this comptues orientations form 0 to 360 degrees.
 %         To obtain orientaiton from 0 to 180, use mod(ori,180).
@@ -12,14 +12,14 @@ function vecLD = computeLength(vecLD)
 if nargin < 2
     forceRecompute = 0;
 end
-if isfield(vecLD,'orientation') & ~forceRecompute
+if isfield(vecLD,'length') && ~forceRecompute
     return
 end
 
+vecLD.length = {};
+vecLD.contourLength = [];
 for c = 1:vecLD.numContours
     thisCon = vecLD.contours{c};
-    numSegments = size(thisCon,1);
-    vecLD.orientation{c} = atan2d((thisCon(:,3)-thisCon(:,1)),(thisCon(:,4)-thisCon(:,2)));
-    isNeg = (vecLD.orientation{c} < 0);
-    vecLD.orientation{c}(isNeg) = vec.orientation{c}(isNeg) + 360; 
+    vecLD.length{c} = sqrt(((thisCon(:,3)-thisCon(:,1)).^2+(thisCon(:,4)-thisCon(:,2)).^2);
+    vecLD.contourLength(c) = sum(vecLD.lengths{c});
 end
