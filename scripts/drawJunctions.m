0f93d8874b0d749b87d5a3fea3140044307bfe64
DirkBWalther <dirk.walther@gmail.com>
2022-03-11 01:57:06 -0500

    functions for drawing line drawings and junctions

---
+++ scripts/drawJunctions.m
@@ -0,0 +1,49 @@
+function drawJunctions(Junctions,types,MarkerSize,colors)
+% drawJunctions(Junctions,types,MarkerSize)
+%
+% Draws the junctions into the current figure.
+%
+% Input:
+%   Junctions - the junctions to be drawn. E.g., from vecLD.junctions.
+%   types - cell array with the types of junctions to be drawn in order
+%           any combination of 'L','T','X','Y','Arrow','Star
+%           default: {} - all junctions
+%   Markersize - the size of the marker for the junctions
+%                default: TBD
+%   colors - Nx3 array of RGB values to be used as colors
+%            default: Matlab's 'lines' color map - the regular order for line plots.
+
+if nargin < 3
+    MarkerSize = 5;
+end
+
+if nargin < 2
+    types = {};
+end
+
+junctionTypes = {Junctions(:).type};
+if isempty(types)
+    types = unique(junctionTypes);
+end
+numTypes = length(types);
+
+if nargin < 4
+    colors = lines(numTypes);
+end
+
+positions = reshape([Junctions(:).position],2,length(Junctions))';
+hold on;
+for t = 1:length(types)
+    typeIdx = strcmp(types{t},junctionTypes);
+    h(t) = plot(positions(typeIdx,1),positions(typeIdx,2),'o',...
+                'MarkerFaceColor',colors(t,:),...
+                'MarkerEdgeColor',colors(t,:),...
+                'MarkerSize',MarkerSize);
+end
+legend(h,types,'Location','NorthEastOutside');
+legend boxoff;
+
+
+
+
+
---
+++ scripts/drawLinedrawing.m
@@ -0,0 +1,19 @@
+function drawLinedrawing(vecLD)
+% drawLinedrawing(vecLD)
+% Draws a line drawing from a data structure into a figure.
+%
+% Input:
+%   vecLD - a line drawing structure
+
+for c = 1:vecLD.numContours
+    thisC = vecLD.contours{c};
+    X = [thisC(:,1);thisC(end,3)];
+    Y = [thisC(:,2);thisC(end,4)];
+    plot(X,Y,'k-');
+    hold on;
+end
+
+axis([1,vecLD.imsize(1),1,vecLD.imsize(2)]);
+axis ij;
+axis equal;
+
