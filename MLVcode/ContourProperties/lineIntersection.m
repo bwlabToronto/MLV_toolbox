eefabc76da31dcba9e60dbc9482acd3082acaa55
DirkBWalther <dirk.walther@gmail.com>
2022-04-29 14:15:24 -0400

    fixed minor bugs in detecting junctions when line segments are colinear

--- MLVcode/ContourProperties/lineIntersection.m
+++ MLVcode/ContourProperties/lineIntersection.m
@@ -19,6 +19,8 @@ function Position = lineIntersection(queryLine,refLine,RE,AE)
 %   If the lines do intersect, the coordintes [x,y] of the intersection point.
 %   Otherwise Position will be empty [].
 
+eps = 1e-4;
+
 Ay = queryLine(3) - queryLine(1);
 Ax = queryLine(4) - queryLine(2);
 By = refLine(3) - refLine(1);
@@ -36,18 +38,33 @@ at=min( RE,  AE/max(abs(Ax),abs(Ay))); %calculate the threshold of the ratio
 bt=min( RE,  AE/max(abs(Bx),abs(By))); %calculate the threshold of the ratio
 
 if (-at<=a) && (a<=1+at) && (-bt <= b) && (b <= 1+bt)
-    A1=queryLine(2)-queryLine(4);
-    B1=queryLine(3)-queryLine(1);
-    C1=queryLine(1)*queryLine(4)-queryLine(2)*queryLine(3);
-
-    A2=refLine(2)-refLine(4);
-    B2=refLine(3)-refLine(1);
-    C2=refLine(1)*refLine(4)-refLine(2)*refLine(3);
-
-    D=A1*B2-A2*B1;
-
-    Position(1)=(B1*C2-B2*C1)/D;
-    Position(2)=(A2*C1-A1*C2)/D;
+    % special cases where a or b are 0 or 1
+    if abs(a) < eps
+        Position = queryLine([1:2]);
+    elseif abs(a-1) < eps
+        Position = queryLine([3:4]);
+    elseif abs(b) < eps
+        Position = refLine([1:2]);
+    elseif abs(b-1) < eps
+        Position = refLine([3:4]);
+    else
+        % general case
+        A1=queryLine(2)-queryLine(4);
+        B1=queryLine(3)-queryLine(1);
+        C1=queryLine(1)*queryLine(4)-queryLine(2)*queryLine(3);
+    
+        A2=refLine(2)-refLine(4);
+        B2=refLine(3)-refLine(1);
+        C2=refLine(1)*refLine(4)-refLine(2)*refLine(3);
+    
+        D=A1*B2-A2*B1;
+    
+        Position(1)=(B1*C2-B2*C1)/D;
+        Position(2)=(A2*C1-A1*C2)/D;
+        if any(Position < 0)
+            keyboard;
+        end
+    end
 else
     Position=[];
 end
