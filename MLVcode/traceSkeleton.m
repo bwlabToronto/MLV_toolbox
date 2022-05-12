1f1c0a81b407de5d93a8bcb84db843fcf94c9031
Morteza Rezanejad <morteza.rezanejad@gmail.com>
2022-05-12 18:33:08 -0400

    Update traceSkeleton

--- MLVcode/traceSkeleton.m
+++ MLVcode/traceSkeleton.m
@@ -1,30 +1,19 @@
-% Copyright Morteza Rezanejad
-% McGill University, Montreal, QC 2019
-%
-% Contact: morteza [at] cim [dot] mcgill [dot] ca 
-% -------------------------------------------------------------------------
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <https://www.gnu.org/licenses/>.
-% -------------------------------------------------------------------------
-
 function allBranches = traceSkeleton(MAT)
-
+% allBranches = traceSkeleton(MAT)
+%   This function traces all branches of Medial Axis Transform (MAT) data structure.
+%
+% Input:
+%   MAT - a given MAT data structure ,
+%   
+% Output:
+%   allBranches - a struct that includes all branches computed from MAT,
+%   this includes X and Y position of each each branch point as well as the
+%   Radius value (radius function) and the average
+%   outward flux value (AOF) along each branch
 
 SegList  = GetConSeg(MAT.skeleton);
-% allBranches = cell(size(SegList));
 allBranches = [];
 
-% branch = [];
 for i = 1 : length(SegList)
     
     
@@ -37,12 +26,8 @@ for i = 1 : length(SegList)
     
     R = MAT.distance_map(C);
     F = MAT.AOF(C);
-%     CC = [X,Y,R,F];
     branch = struct('X',X,'Y',Y,'Radius',R,'AOF',F);
     allBranches = [allBranches;branch];
-%     allBranches{i} = CC;
 end
-
-% allBranches = allBranches(~cellfun('isempty',allBranches));
 
 end
